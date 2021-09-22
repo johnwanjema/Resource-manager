87cd485f49ccdcc20dfcaa52ffd618399505d084
wanjema <jonwanjema@gmail.com>
2021-09-22 18:23:35 +0300

    setup home component

---
+++ resources/js/routes/home.js
@@ -0,0 +1,9 @@
+const home = () =>import ( '../components/home')
+
+export default [
+    {
+        path: '/home',
+        component: home,
+        name: 'home',
+    },
+]
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// welcome
+// Auth::routes();
+
+Route::get('/{any?}/{component?}', function () {
+    return view('home');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
