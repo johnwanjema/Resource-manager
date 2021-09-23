09350412a56749ba540ccd4c7d807da58a0a9231
wanjema <jonwanjema@gmail.com>
2021-09-23 15:17:58 +0300

    add nav links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                <router-view></router-view>   
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container-fluid">
+    <router-view></router-view>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,35 +19,55 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .nav-link {
+            font-size: 1.0rem;
+            color: black;
+        }
+
+        .nav-item a:active {
+            color: greenyellow;
+        }
+    </style>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                   Resource Manager
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <!-- <div class="container"> -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Resource Manager
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <router-link class="nav-link" to="/home/">PDF <span class="sr-only">(current)</span></router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/home/htmlsnippets">HTML snippet</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/home/links">Links</router-link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ">
+                    <li class="nav-item my-lg-0">
+                        <a class="nav-link" href="/login">Login as Admin</a>
+                    </li>
+                </ul>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                    </ul>
-                </div>
             </div>
+            <!-- </div> -->
         </nav>
 
-        <main class="py-4">
+        <main class="py-3">
             @yield('content')
         </main>
     </div>
 </body>
+
 </html>
