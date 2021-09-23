40bca8af81ab89c657492031b2ffff0caad9825f
wanjema <jonwanjema@gmail.com>
2021-09-23 15:25:22 +0300

    create props for home component

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -1,3 +1,52 @@
 <template>
-    <div>Home component </div>
+    <div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">
+                            With supporting text below as a natural lead-in to additional content.
+                        </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">
+                            With supporting text below as a natural lead-in to additional content.
+                        </p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    props: ["category"],
+    data() {
+        return {};
+    },
+    methods:{
+        getData(){
+            console.log('data');
+             axios.get("/api/gethomedata" )
+                .then(({data}) => {
+                    console.log(data);
+                })
+                .catch((e) => {
+                    console.log(error)
+                });
+        }
+    },
+    created(){
+        this.getData();
+    }
+};
+</script>
--- resources/js/routes/home.js
+++ resources/js/routes/home.js
@@ -2,8 +2,13 @@ const home = () =>import ( '../components/home')
 
 export default [
     {
-        path: '/home',
+        path: '/home/:category',
         component: home,
         name: 'home',
+        props: true
+    },
+    {
+        path: '/home',
+        redirect:'/home/pdf'
     },
 ]
